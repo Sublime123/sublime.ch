e575d7bcfd684962eedddf0837fc1b824693ba6f
sub <sublimecharacter123@gmail.com>
2022-01-29 14:52:31 +0300

    Test of get form

--- sub/mainBoard/views.py
+++ sub/mainBoard/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'home.html', {'name':'Navalny'})
+def add(request):
+    val1 = request.GET['one']
+    val2 = request.GET['two']
+    res = str(int(val1) + int(val2))
+    return render(request, 'result.html', {'result':res})
