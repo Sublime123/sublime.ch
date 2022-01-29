6f76abd242c4a82fffd818a2df3ef4675f543106
sub <sublimecharacter123@gmail.com>
2022-01-29 20:25:29 +0300

    post

--- sub/mainBoard/views.py
+++ sub/mainBoard/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request,'home.html', {'name':'Navalny'})
 def add(request):
-    val1 = request.GET['one']
-    val2 = request.GET['two']
+    val1 = request.POST['one']
+    val2 = request.POST['two']
     res = str(int(val1) + int(val2))
     return render(request, 'result.html', {'result':res})
