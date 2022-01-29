0faed20a9d0aef2b76ed3814749f7385a8c939b3
sub <sublimecharacter123@gmail.com>
2022-01-29 14:40:07 +0300

    fix

--- sub/mainBoard/views.py
+++ sub/mainBoard/views.py
@@ -2,8 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-<<<<<<< HEAD
     return render(request,'home.html', {'name':'Navalny'})
-=======
-    return HttpResponse("Hello, world. You're at the polls index.")
->>>>>>> 49393aa0ff201196dcddf1d84202c0b5f587d2f4
