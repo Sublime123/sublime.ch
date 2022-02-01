ce575d8d71462271633d03dcb582c91b9dc555e6
sub <sublimecharacter123@gmail.com>
2022-02-01 13:24:00 +0300

    Updated posting, half done multypage

--- sub/mainBoard/models.py
+++ sub/mainBoard/models.py
@@ -1,16 +1,20 @@
 from email.policy import default
 from types import NoneType
 from django.db import models
-from django.contrib.auth.models import User
-
+#from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
 
 class Post(models.Model):    
-    number = models.AutoField(primary_key=True,default=0) 
+    number = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256,null=True, blank=True)
     msg = models.CharField(max_length=1000,null=True, blank=True)
     img = models.ImageField(upload_to='pics',null=True, blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default=NoneType)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True
+    )
     published_date = models.DateTimeField(null=True, blank=True)
     def __str__(self):
-        return self.name
+        return self.msg
--- sub/mainBoard/views.py
+++ sub/mainBoard/views.py
@@ -1,18 +1,31 @@
 from cgitb import text
+from types import NoneType
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 from django.http import HttpResponseRedirect
 from .forms import PostForm
 from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
 
 def index(request):
     return render(request,'home.html')
 
+def getMessagesForScreen(listOfAll,screen,postsPerScreen):
+    result = []
+    for idx,val in enumerate(listOfAll):
+        if idx >= screen * postsPerScreen and idx < (screen + 1) * postsPerScreen:
+            result.append(val)
+    return result
+
+
 def board(request):
     form = PostForm()
-    messages = []
-    messages = list(Post.objects.order_by('number')[:50])
+    posts_per_screen = 50
+    listOfAll = list(Post.objects.order_by('-number'))
+    screen = request.GET.get("screen", 0)
+    messages = getMessagesForScreen(listOfAll,screen,posts_per_screen)
+    #print(messages)
     return render(request, 'board.html', {'form': form,'messages':messages})
     """
     posts = []
@@ -28,10 +41,9 @@ def send(request):
     form = PostForm(request.POST)
     if form.is_valid():
         post = form.save(commit=False)
-        post.user = request.user
+        post.user = request.user if request.user.is_anonymous == False else None
         post.published_date = timezone.now()
         post.save()
-        print(post)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     """
     if request.method == 'POST':
