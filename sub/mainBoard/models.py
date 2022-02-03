71969427b1575ff48c6f5f6418b9ed6137bcdb2a
sub <sublimecharacter123@gmail.com>
2022-02-03 11:24:45 +0300

    Image thumbnails finished

--- sub/mainBoard/models.py
+++ sub/mainBoard/models.py
@@ -3,12 +3,14 @@ from django.db import models
 #from django.contrib.auth.models import User
 from django.conf import settings
 # Create your models here.
+def only_filename(instance, filename):
+    return filename
 
 class Post(models.Model):    
     number = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256,default="",blank=True)
     msg = models.CharField(max_length=1000,default="",blank=True)
-    img = models.ImageField(upload_to='pics',null=True, blank=True)
+    img = models.ImageField(upload_to=only_filename,null=True, blank=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
