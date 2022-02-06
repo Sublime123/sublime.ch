ea38d8d7eb79a43d1007fc602a43fa82a3542e6d
sub <sublimecharacter123@gmail.com>
2022-02-06 20:11:11 +0300

    Threads update - 90% (needs to test )

--- sub/mainBoard/urls.py
+++ sub/mainBoard/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('board', views.board, name='board'),
     path('thread', views.thread, name='thread'),
+    path('board', views.board, name='board'),
+    #path('thread', views.thread, name='thread'),
+    
     #path('send', views.send, name='send'),
     #path('add', views.add, name='add')
 ]
--- sub/mainBoard/views.py
+++ sub/mainBoard/views.py
@@ -10,6 +10,9 @@ from .forms import ThreadForm
 from django.utils import timezone
 from django.contrib.auth.models import AnonymousUser
 from math import floor, ceil
+from django.http import HttpResponseNotFound
+from django.db.models import Max
+
 def index(request):
     return render(request,'home.html')
 
@@ -31,9 +34,18 @@ def board(request):
             post.published_date = timezone.now()
             post.pic = request.FILES.get('pic')
             post.opPost = True
-            thread = Threads()            
+            thread = Threads()
+            #
+            #thread = Threads.objects.all().filter(threadNumber=threadId)
+            bumpCounter = Threads.objects.aggregate(Max('bump'))
+            if bumpCounter['bump__max'] is not None:
+                bumpCounter = bumpCounter['bump__max'] + 1
+            else:
+                bumpCounter = 0
+            thread.bump = bumpCounter
             thread.save()
-            post.thread = thread
+            #            
+            post.thread = thread            
             post.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     elif request.method =='GET':
@@ -51,18 +63,19 @@ def board(request):
         return render(request, 'board.html', data)
         """
         th = []
-        threads = list(Threads.objects.order_by('bump'))
+        threads = list(Threads.objects.order_by('-bump'))
         posts_per_screen = 5
         try:
             screen = int(request.GET.get("screen", 0))
         except:
             screen = 0
         maxScreen, threads = getMessagesForScreen(threads,screen,posts_per_screen)
-        for thread in threads:    
+        for thread in threads:
             oppost = list(Post.objects.filter(thread=thread).filter(opPost=True))
-            posts = list(Post.objects.filter(thread=thread).filter(opPost=False).order_by('-number'))
+            posts = list(Post.objects.filter(thread=thread).filter(opPost=False).order_by('number'))
+            posts = posts[len(posts) - 5:len(posts)]
             if len(oppost) > 0:
-                th.append({'op':oppost[0], 'posts':posts})
+                th.append({'op':oppost[0], 'op_link':'thread?t=' + str(oppost[0].thread.threadNumber), 'posts':posts})
         screens = []
         for n in range(maxScreen):
             screens.append({'screen':n, 'current': True if n == screen else False})
@@ -72,14 +85,32 @@ def board(request):
         return render(request, 'board.html', data)
 
 def thread(request):
-    form = ThreadForm(request.POST, request.FILES)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.user = request.user if request.user.is_anonymous == False else None
-        post.published_date = timezone.now()
-        post.pic = request.FILES.get('pic')
-        post.save()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    if request.method =='POST':
+        form = ThreadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user if request.user.is_anonymous == False else None
+            post.published_date = timezone.now()
+            post.pic = request.FILES.get('pic')
+            post.opPost = False
+            threadId=request.POST.get('threadId')
+            thread = Threads.objects.all().filter(threadNumber=threadId)
+            bump = thread.aggregate(Max('bump'))
+            bump = bump['bump__max'] + 1
+            thread.update(bump=bump)
+            post.thread = list(thread)[0]
+            post.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    elif request.method =='GET':
+        threadId = int(request.GET.get("t", 0))
+        form = PostForm()
+        if threadId > 0:
+            oppost = list(Post.objects.filter(thread=threadId).filter(opPost=True))
+            posts = list(Post.objects.filter(thread=threadId).filter(opPost=False).order_by('number'))
+            data = {'oppost':oppost[0], 'posts':posts, 'threadId':threadId,'form':form}
+            return render(request, 'thread.html', data)
+        return HttpResponseNotFound("<h1>404</h1>")         
 
 def send(request):
     form = PostForm(request.POST, request.FILES)
